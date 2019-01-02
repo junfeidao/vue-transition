39b2c7e65ca230014d9c81e7fa85f73614a34232
junfeidao <823201662@qq.com>
2019-01-02 11:37:33 +0800

    feat(components/): 添加翻译和获取翻译结果页面

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="translateForm">
+    <form v-on:submit="formSubmit">
+      <input type="text" v-model="textToTranslate" placeholder="请输入需要翻译的内容">
+      <select v-model="language">
+        <option value="en">English</option>
+        <option value="ru">Russian</option>
+        <option value="ko">Korean</option>
+        <option value="ja">Janpenese</option>
+      </select>
+      <input type="submit" value="翻译">
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'translateForm',
+    data: function() {
+      return {
+        textToTranslate: '',
+        language: 'en'
+      }
+    },
+    methods: {
+      formSubmit: function(e) {
+        // alert(this.textToTranslate);
+        this.$emit('formSub', this.textToTranslate, this.language);
+        e.preventDefault();
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
